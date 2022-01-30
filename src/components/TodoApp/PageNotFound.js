a8fc9eb7d93c86e4413c1bc16d5008003c219eee
unknown <yaswanth456p@gmail.com>
2022-01-30 22:00:37 +0530

    created Error page

--- src/components/TodoApp/PageNotFound.js
+++ src/components/TodoApp/PageNotFound.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React from "react";
+import "./styles.css";
+import ErrorImage from "../../assets/404_Error_image.svg";
+import { Link } from "react-router-dom";
 
-const PageNotFound = ({props}) => {
-  return <h1>404 - Page Not Found</h1>;
+const PageNotFound = ({ props }) => {
+  return (
+    <div className="w-100 h-100 all-center">
+      <img
+        src={ErrorImage}
+        alt="page-not-found"
+        className="page-not-found-image"
+      />
+      <div className="t-center all-center">
+        <h1 className="error-page-heading">Oops.. Page Not Found</h1>
+        <p className="f-24">
+          We can’t seem to find the page you’re looking for.
+        </p>
+        <Link to="/dashboard">
+          <button className={"blue-button loading-btn error-page-button"}>
+            Return to Dashboard
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default PageNotFound;
