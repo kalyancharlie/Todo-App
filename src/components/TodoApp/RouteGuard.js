d1cf14f58ad45193e757a679c3d7ade07123454f
unknown <yaswanth456p@gmail.com>
2022-01-31 18:53:46 +0530

    Completed Please wait page

--- src/components/TodoApp/RouteGuard.js
+++ src/components/TodoApp/RouteGuard.js
@@ -4,6 +4,7 @@ import { useNavigate, useLocation } from "react-router-dom";
 import { isTokenExpired, verifySession } from "../../utils/AuthUtil";
 import { renewToken, verifyToken } from "../../api/Api";
 import { TODO_LOGIN, TODO_SESSION_EXPIRED } from "../../constants/constants";
+import { ThreeDots } from "react-loader-spinner";
 
 const RouteGuard = ({ children }) => {
   const navigate = useNavigate();
@@ -40,8 +41,9 @@ export default RouteGuard;
 
 const Loading = () => {
   return (
-    <>
-      <h1>Please wait.....</h1>
-    </>
+    <div className="all-center w-100 h-100">
+      <h1>Please wait...</h1>
+      <ThreeDots heigth="100" width="100" color="#15184d" ariaLabel="loading" />
+    </div>
   );
 };
